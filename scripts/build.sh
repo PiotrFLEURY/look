f2011d69c0f2eeafc2c7f98217485dd4174b027d
PiotrFLEURY <piotr.fleury@gmail.com>
2022-09-11 11:45:53 +0200

    chore(build): add build VsCode task

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,6 +8,12 @@ cd look
 flutter pub get
 cd ..
 
+cd look/example
+flutter pub get
+flutter pub run build_runner build --delete-conflicting-outputs
+flutter build web
+cd ../..
+
 # build look_generator package then
 
 cd look_generator
