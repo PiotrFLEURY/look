94051998f5201815ea71ec98467f5393b435abd8
PiotrFLEURY <piotr.fleury@gmail.com>
2022-09-11 12:22:53 +0200

    docs(dart): add dart documentation to sources

--- look/lib/look.dart
+++ look/lib/look.dart
@@ -8,6 +8,7 @@ const Look look = Look();
 /// Annotation used to generate a runnable Flutter app containning only the Widget to look
 @Target({TargetKind.classType})
 class Look {
+  /// Method reference to the Widget builder function
   final String? builder;
   const Look([
     this.builder,
@@ -17,12 +18,25 @@ class Look {
 /// Annotation used to generate a golden test for the Widget to look
 @Target({TargetKind.function})
 class LookGolden {
+  /// Name of the golden file
   final String? name;
+
+  /// DartType of the Widget to look
   final Type? type;
+
+  /// Method reference to the Widget builder function
   final Function? builder;
+
+  /// Method reference to the light theme builder function
   final Function? lightTheme;
+
+  /// Method reference to the dark theme builder function
   final Function? darkTheme;
+
+  /// List of every dimension permutations to test (format: widthxheight)
   final List<String> dimensions;
+
+  /// Annotation constructor
   const LookGolden({
     required this.type,
     this.name,
