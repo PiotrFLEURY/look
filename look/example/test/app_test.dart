40e8c0b668c268919fede6c64f66fa775cb4fd3f
PiotrFLEURY <piotr.fleury@gmail.com>
2022-09-11 12:25:52 +0200

    fix(build): remove unused import

--- look/example/test/app_test.dart
+++ look/example/test/app_test.dart
@@ -2,7 +2,6 @@ import 'package:example/app.dart';
 import 'package:look/look.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'home_page_test.dart';
 
 part 'app_test.lookgolden.dart';
 
